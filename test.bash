18ed3bf9dff729f83640a1eae8583e4b6e819e2b
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-10-12 11:43:11 +0900

    Rename

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash -xv
 
-out=$(seq 5 | ./plus)
+ng () {
+	echo NG at line $1
+	res=1
+}
 
-[ "${out}" = 15 ]
+res=0
+
+### I/O test ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
