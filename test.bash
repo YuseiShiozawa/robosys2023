03e2941ca90a24e895a51c778a31788fe7f65240
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-10-18 23:40:12 +0900

    modified

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
-#!/bin/bash -xv
+#!/bin/bash 
+#SPDX-FileCopyrightText: 2023 Yusei Shiozawa
+#SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at line $1
@@ -10,6 +12,17 @@ res=0
 ### I/O test ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng $LINENO
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+
 
 [ "$res" = 0 ] && echo OK
 
