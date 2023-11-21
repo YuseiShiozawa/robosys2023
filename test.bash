633e4f8194ae15df06321baaf6626b1de51ecdda
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-21 10:11:31 +0900

    add test sentence5

--- test.bash
+++ test.bash
@@ -36,6 +36,10 @@ out=$(echo seq 5 | ./plus)
 [ "$?" = 1 ] || ng $LINENO
 [ "${out}" = "" ] || ng $LINENO
 
+out=$(seq 1 2 2 5 | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
 [ "$res" = 0 ] && echo OK
 
 exit $res
